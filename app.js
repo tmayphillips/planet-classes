3d7f3c6985f7bb331cb24e0d590705fa8e5e32e4
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2021-12-02 13:47:37 -0600

    types for information, added event listeners

---
+++ app.js
@@ -0,0 +1,112 @@
+"use strict";
+const submitPasswordBtn = document.getElementById('submit-password-btn');
+const planetListDiv = document.getElementById('planet-list');
+const addPlanetDiv = document.getElementById('add-planet');
+class Planet {
+    constructor(name, size, parentStar) {
+        this.name = name;
+        this.size = size;
+        this.parentStar = parentStar;
+    }
+}
+Planet.listOfPlanets = [];
+Planet.inventoryCount = 2000000000;
+Planet.purchasedPlanets = [];
+class GasGiant extends Planet {
+    constructor(name, size, parentStar) {
+        super(name, size, parentStar);
+    }
+}
+class IceGiant extends Planet {
+    constructor(name, size, parentStar) {
+        super(name, size, parentStar);
+    }
+}
+class RockyPlanet extends Planet {
+    constructor(name, size, parentStar) {
+        super(name, size, parentStar);
+    }
+}
+class Person {
+}
+function purchaseStar() {
+    // private variables
+    // private _planetName:string
+    Planet.inventoryCount--;
+}
+function displayInventoryForm() {
+    addPlanetDiv.style.display = 'none';
+    console.log(`Add password button pressed. Planet List: ${Planet.listOfPlanets}`);
+    planetListDiv.innerHTML += `
+        <label for="planet-name">Planet Name:</label>
+        <input type="text" id="planet-name" name="planet-name"><br>
+        <select name="planet-type" id="planet-type">
+            <option>Select planet type</option>
+            <option value="rocky">Rocky Planet</option>
+            <option value="gas">Gas Giant</option>
+            <option value="ice">Ice Giant</option>
+        </select>
+    `;
+    const typeSelect = document.getElementById('planet-type');
+    console.log(typeSelect.value);
+    typeSelect.addEventListener('change', event => {
+        if (typeSelect.value === 'gas') {
+            planetListDiv.innerHTML += `
+                <div>
+                    <input type="checkbox" id="oxygen" name="oxygen">
+                    <label for='oxygen'>Oxygen</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="helium" name="helium">
+                    <label for='helium'>Helium</label>
+                </div>
+                <div>
+                    <input type="checkbox" id="hydrogen" name="hydrogen">
+                    <label for='hydrogen'>Hydrogen</label>
+                </div>
+            `;
+        }
+    });
+    // <label for="planet-name">Planet Name:</label>
+    // <input type="text" id="planet-name" name="planet-name"><br>
+    // <select name="planet-type" id="planet-types">
+    // <option value="rocky">Rocky Planet</option>
+    // <option value="gas">Gas Giant</option>
+    // <option value="ice">Ice Giant</option>
+    // </select>
+    // add additional sections based on dropdown
+    // submit and call getInputValues()
+}
+function getInputValues() {
+    // get input values
+    // check if name is already taken
+    // create new Planet based on class
+}
+function createNewPlanet(name, type, info) {
+    let newPlanet;
+    if (type === 'rocky') {
+        newPlanet = new RockyPlanet(name, info.size, info.parentStar);
+        Planet.listOfPlanets.push(newPlanet);
+    }
+    if (type === 'gas') {
+        newPlanet = new GasGiant(name, info.size, info.parentStar);
+        Planet.listOfPlanets.push(newPlanet);
+    }
+    if (type === 'ice') {
+        newPlanet = new IceGiant(name, info.size, info.parentStar);
+        Planet.listOfPlanets.push(newPlanet);
+    }
+}
+createNewPlanet('Mercury', 'rocky', { size: 1516, parentStar: 'Sol' });
+// let mercury = new Rocky 
+// let venus = new Rocky
+// let earth = new Rocky
+// let mars = new Rocky
+// let jupiter = new GasGiant
+// let saturn = new GasGiant
+// let uranus = new IceGiant
+// let neptune = new IceGiant
+submitPasswordBtn === null || submitPasswordBtn === void 0 ? void 0 : submitPasswordBtn.addEventListener('click', event => {
+    event.preventDefault();
+    displayInventoryForm();
+});
--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
+const submitPasswordBtn = document.getElementById('submit-password-btn')
+const planetListDiv = document.getElementById('planet-list')
+const addPlanetDiv = document.getElementById('add-planet')
+
 class Planet {
     public static listOfPlanets:Planet[] = []
-    public static inventoryCount: number = 2000000000
-    public static purchasedPlanets = []
+    public static inventoryCount:number = 2000000000
+    public static purchasedPlanets:string[] = []
 
     name:string
     size:number
@@ -13,20 +17,11 @@ class Planet {
         this.parentStar = parentStar
     }
 
-
-    // general info
-        // name
-        // size
-        // parent star
     
-    // purchase function - protected method
+    
 
         
-        // private variables
-        // private _planetName:string
         
-
-        // Planet.inventory--
 
     //  planet display function - protected method
         // event listener - submit btn
@@ -63,7 +58,56 @@ class Person {
     // private _lastName:string
 }
 
+function purchaseStar () { // protected method
+    // private variables
+    // private _planetName:string
+    
+
+    Planet.inventoryCount--
+}
+
 function displayInventoryForm() {
+    addPlanetDiv.style.display = 'none'
+
+    console.log(`Add password button pressed. Planet List: ${Planet.listOfPlanets}`)
+
+    planetListDiv.innerHTML += `
+        <label for="planet-name">Planet Name:</label>
+        <input type="text" id="planet-name" name="planet-name"><br>
+        <select name="planet-type" id="planet-type">
+            <option>Select planet type</option>
+            <option value="rocky">Rocky Planet</option>
+            <option value="gas">Gas Giant</option>
+            <option value="ice">Ice Giant</option>
+        </select>
+    `
+
+    const typeSelect = document.getElementById('planet-type')
+    console.log((<HTMLSelectElement>typeSelect).value)
+
+    typeSelect.addEventListener('change', event => {
+        if ((<HTMLSelectElement>typeSelect).value === 'gas') {
+            planetListDiv.innerHTML += `
+                <div id='gas-mixture' class='gas-mixtuer'>
+                    <div>
+                        <input type="checkbox" id="oxygen" name="oxygen">
+                        <label for='oxygen'>Oxygen</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="helium" name="helium">
+                        <label for='helium'>Helium</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" id="hydrogen" name="hydrogen">
+                        <label for='hydrogen'>Hydrogen</label>
+                    </div>
+                </div>
+            `
+        }
+    })
+
+    
+
     // <label for="planet-name">Planet Name:</label>
     // <input type="text" id="planet-name" name="planet-name"><br>
     // <select name="planet-type" id="planet-types">
@@ -84,10 +128,27 @@ function getInputValues() {
     // create new Planet based on class
 }
 
-interface Info {
+interface gasInfo {
     size:number
     parentStar:string
+    gases:string[]
 }
+
+interface rockyInfo {
+    size:number
+    parentStar:string
+    coreComponents:string[]
+}
+
+interface iceInfo {
+    size:number
+    parentStar:string
+    fluidElements:string[]
+}
+
+type Info = gasInfo | rockyInfo | iceInfo
+
+
     
 
 function createNewPlanet (name:string, type:string, info:Info): void {
@@ -109,7 +170,7 @@ function createNewPlanet (name:string, type:string, info:Info): void {
     
 } 
 
-createNewPlanet ('Mercury', 'rocky', {size: 1516, parentStar: 'Sol'})
+createNewPlanet ('Mercury', 'rocky', {size: 1516, parentStar: 'Sol', coreComponents:['iron']})
 
 // let mercury = new Rocky 
 // let venus = new Rocky
@@ -120,5 +181,10 @@ createNewPlanet ('Mercury', 'rocky', {size: 1516, parentStar: 'Sol'})
 // let uranus = new IceGiant
 // let neptune = new IceGiant
 
+
+submitPasswordBtn?.addEventListener('click', event => {
+    event.preventDefault()
+    displayInventoryForm()
+})
 
 
