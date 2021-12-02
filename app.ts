f1f934873ddf0d20f5a63a663faa13edbc8a0a5b
Tiffany May-Phillips <tiffany.phillips12@gmail.com>
2021-12-02 11:54:30 -0600

    first commit

---
+++ app.ts
@@ -0,0 +1,88 @@
+class Planet {
+    public static listOfPlanets:Planet = []
+    public static inventoryCount: number = 2000000000
+    public static purchasedPlanets = []
+
+    // general info
+        // name
+        // size
+        // parent star
+    
+    // purchase function - protected method
+
+        
+        // private variables
+        // private _planetName:string
+        
+
+        // Planet.inventory--
+
+    //  planet display function - protected method
+        // event listener - submit btn
+
+        // <div id = 'mercury'>
+        //     <img src='mercury.jpg' />
+        //     <h2 class='planet-name'>Mercury</h2>
+        //     <h3 class='planet-type'>Rocky</h3>
+        //     <p>Buy Mercury. It's great!</p>
+        // </div>
+}
+    
+class GasGiant extends Planet { // inheritance
+    // receive a sample gas mixture - private function
+}
+
+class IceGiant extends Planet {
+
+}
+
+class RockyPlanet extends Planet {
+
+}
+    
+class Person {
+    // private _firstName:string
+    // private _lastName:string
+}
+
+function displayInventoryForm() {
+    // <label for="planet-name">Planet Name:</label>
+    // <input type="text" id="planet-name" name="planet-name"><br>
+    // <select name="planet-type" id="planet-types">
+        // <option value="rocky">Rocky Planet</option>
+        // <option value="gas">Gas Giant</option>
+        // <option value="ice">Ice Giant</option>
+    // </select>
+
+    // add additional sections based on dropdown
+
+    // submit and call getInputValues()
+
+}
+
+function getInputValues() {
+    // get input values
+    // check if name is already taken
+    // create new Planet based on class
+}
+    
+
+function createNewPlanet (name<string>, type<string>): void {
+    if (type === 'rocky') {
+        let newPlanet = new Rocky(name)
+    }
+    Planet.listOfPlanets.push(newPlanet)
+} 
+
+
+// let mercury = new Rocky 
+// let venus = new Rocky
+// let earth = new Rocky
+// let mars = new Rocky
+// let jupiter = new GasGiant
+// let saturn = new GasGiant
+// let uranus = new IceGiant
+// let neptune = new IceGiant
+
+
+
